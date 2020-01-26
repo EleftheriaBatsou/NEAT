442b7820b6b197a8383c6b6222811a81acf1aef6
anastasiaZer <anastasia.zervou@teamcmp.com>
2020-01-26 14:12:00 +0100

    update FE

--- www/js/index.js
+++ www/js/index.js
@@ -6,54 +6,70 @@
 
 //listeners
 var registerbutton = document.getElementById("registerbutton");
-registerbutton.addEventListener("click",registerbutton_click);
+if(registerbutton) {
+    registerbutton.addEventListener("click",registerbutton_click);
+}
 
 //-------------------------------
+var password1 = document.getElementById("password_1");
+if(password1) {
+    password1.addEventListener('input', event => {
+        event.preventDefault();
+        var password = document.getElementById("password").value;
+        var password1 = document.getElementById("password_1").value;
+        var errorMessage = document.getElementById('dbresponse');
 
+        if(password && password1 && (password === password1)) {
+            document.getElementById("dbresponse").innerHTML = ' ';
+            registerbutton.classList.remove('disabled');
+        } else {
+            document.getElementById("dbresponse").innerHTML = ' ';
+            errorMessage.innerHTML += 'Ο κωδικός πρόσβασης και ο κωδικός επιβεβαίωσης πρέπει να είναι οι ίδιοι.';
+
+        }
+    });
+}
 
 function registerbutton_click(){
-var username = document.getElementById("username").value.toString();
-var email = document.getElementById("email").value.toString();
-var password= document.getElementById("password").value.toString();
-//window.alert(username+" "+email+" "+password);
+    var username = document.getElementById("username").value.toString();
+    var email = document.getElementById("email").value.toString();
+    var password= document.getElementById("password").value.toString();
+    //window.alert(username+" "+email+" "+password);
 
-var request = new XMLHttpRequest();
-request.onreadystatechange= function(){
-   if (this.readyState == 4 && this.status == 200){
-       document.getElementById("dbresponse").innerHTML = this.responseText;
-   }
-   else{
-       document.getElementById("dbresponse").innerHTML = "Oops! Something went really bad...";
-   }
-};
+    var request = new XMLHttpRequest();
+    request.onreadystatechange= function(){
+       if (this.readyState === 4 && this.status === 200){
+           document.getElementById("dbresponse").innerHTML = this.responseText;
+       }
+       else{
+           document.getElementById("dbresponse").innerHTML = "Oops! Something went really bad...";
+       }
+    };
 
-request.open("GET","https://neatecologicalapp.000webhostapp.com/createuser.php?username="+username+'&email='+email+'&password='+password,true);
-request.send();
+    request.open("GET","https://neatecologicalapp.000webhostapp.com/createuser.php?username="+username+'&email='+email+'&password='+password,true);
+    request.send();
 
 }
 
 function signButton_click(){
-    //window.alert("YESSSS");
-    
-var username = document.getElementById("signusername").value.toString();
-var password= document.getElementById("signpassword").value.toString();
-//window.alert(username+"  "+password);
 
-var request = new XMLHttpRequest();
-request.onreadystatechange= function(){
-   if (this.readyState === 4 && this.status === 200){
-       msg=this.responseText;
-       if(msg == 1){
-           window.location.href="profile.html";
+    var username = document.getElementById("signusername").value.toString();
+    var password= document.getElementById("signpassword").value.toString();
+    //window.alert(username+"  "+password);
+
+    var request = new XMLHttpRequest();
+    request.onreadystatechange= function(){
+       if (this.readyState === 4 && this.status === 200){
+           msg=this.responseText;
+           if(msg == 1){
+               window.location.href="profile.html";
+           }
+           else{
+               window.alert("Δεν βρέθηκε ο συγκεκριμένος χρήστης...");
+           }
        }
-       else{
-           window.alert("User not found...");
-       }
-   }
-};
+    };
 
-request.open("GET","https://neatecologicalapp.000webhostapp.com/login.php?username="+username+'&password='+password,true);
-request.send();
-    
-
+    request.open("GET","https://neatecologicalapp.000webhostapp.com/login.php?username="+username+'&password='+password,true);
+    request.send();
 }
